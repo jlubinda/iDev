fc5e609fc574529c4db66fc8fd81f8b43565411e
jlubinda <jlubinda@gmail.com>
2018-04-08 06:11:04 +0200

    Add files via upload

---
+++ apps/feeds/rest/index.php
@@ -0,0 +1,9 @@
+<?php 
+include find_file("apps/blog/services/rest/index.php");
+include find_file("apps/chat/services/rest/index.php");
+include find_file("apps/support/services/rest/index.php");
+include find_file("apps/website/services/rest/index.php");
+include find_file("apps/uestimate/services/rest/index.php");
+include find_file("apps/wallet/services/rest/index.php");
+include find_file("apps/wiki/services/rest/index.php");
+?>
