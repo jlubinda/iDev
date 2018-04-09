df028571546d209b34960e6227f395b189dcecab
jlubinda <jlubinda@gmail.com>
2018-04-09 09:00:40 +0200

    Update terms.php

--- apps/website/layouts/pages/terms.php
+++ apps/website/layouts/pages/terms.php
@@ -1,104 +1,12 @@
-
-	 
-	 
-
-
-<div class="container" >
-	<br>
-	<br>
-	
-	<div class="row">
-		<div class="col s12 m10 offset-m1 l9 offset-l1">
-
-		<h4 class="header center grey-text text-darken-4">STANDARD TRADING CONDITIONS</h3>
-		<hr>
-<article>
-<p>When you hire a vehicle from  City Drive the folowing are the terms and conditions.</p>
-
-<p> <strong> 1.0	RENTAL DAYS  </strong> <br>
-City Drive will rent out its vehicles at a daily charge. This fee will vary according to the type of vehicle required by a client. The minimum charge will be
-based on one calendar day and not 24hrs.Days for which the car is rented out are counted starting from the first calendar day when the car is collected. This is regardless
-of the time the car is collected on the first calendar day.</p>
-
-<p>  <strong> 2.0	ELIGIBILITY </strong>  <br>
-The company will accept reservations from clients with a valid driver’s license which should have been held for at least two years and upon submission of essential personal
-information i.e. NRC NO, physical address, Contact details, Referees, Place of work. Vehicles will not be rented out to underage individuals below the age of 22 years.</p>
-
-<p> <strong> 3.0	RESCUE AND LOSS OF USE </strong> <br>
-3.1 In the event of City Drive having to do a rescue and upon confirmation of the breakdown having occurred, all towing charges over and above the insurance cover of K600.00
-within Lusaka and K3,500.00 outside Lusaka and the associated costs of the rescue e.g. fuel costs will be borne by the client.</p>
-
-<p> 3.2 In the event of an accident City Drive reserves the right to charge loss of business use to the client for the period when the car will not be in use by Vehicle Portal as a revenue generating asset.</p>
-
-<p> <strong>4.0	VEHICLE CONDITION AND INSPECTION</strong> <br>
-4.1 Clients are expected to inspect the car for any damages/abnormalities before collecting the car. Such inspections should be done in the presence of both the client and Vehicle
-Portal representative. Any abnormalities /damages should be noted and both the client and the Vehicle Portal representative should sign on the inspection form.</p>
-
-<p>4.2 Fuel is the client's responsibility. City Drive reserves the right to provide the car with or without fuel depending on circumstances. In the event that fuel is provided
-the client is expected to return the car with the same amount of fuel which was provided with the car.</p>
-
-<p><strong>5.0	VEHICLE INSURANCE</strong> <br>
-5.1 All vehicles will be provided in good condition. Apart from reasonable wear and tear clients are expected to return the vehicle in the same good condition. Whilst our vehicles
-are comprehensively insured, any damages or loss of vehicle property whilst the car is in the clients' custody and the associated cost of repair will be borne by the client over and
-above the insurance cover. This means the client will cover 10% of the damage costs with a minimum of K1, 000.00</p>
-<p>Any damages amounting to K1, 000.00 and below equaling the minimum excess insurance payable will be borne by the client in their entirety. The 10% excess payable by the client will
-also apply in the event of a write off. The vehicle will be deemed to be in the clients' custody upon collection and handing over keys to the client.</p>
-<p>5.2 This 10% excess on damages and on write-offs is payable immediately the accident happens.</p>
-
-
-<p>6.0	<strong>SECURITY DEPOSIT</strong> <br>
-
-City Drive will accept security deposit equal to the minimum insurance excess of K1, 000.00.This has to be paid in cash or credit card. This deposit is refundable upon vehicle
-return provided they are no damages to the car. If they are damages then Vehicale Portal will deduct an amount to cover the damages and refund the difference. If the cost of damages
-is above the K1, 000.00 then the client will top up the difference.</p>
-
-<p> <strong> 7.0	VEHICLE RETURN</strong> <br>
-All vehicles are expected to be returned on time. Any late returns will attract a penalty equal to a full days charge. An allowance of 1 hour from the time of return is given.
-If the client decides to extend the time of the hire period whilst the vehicle is in the client's custody, then the client should communicate this. In this case there shall be
-an additional charge for the extended hire period. Extension of rental period is not automatic hence clients are advised to communicate on time. In the event of an allowance
-given to bring back the car on or before 10am the following day, any car brought back after 11am will attract a full days charge.</p>
-
-<p> <strong> 8.0 RESERVATION AND CANCELLATION POLICY</strong>
-<br>
-8.1 Any cancellation of the vehicle reservation will attract a Fee 50% of the total quote, invoice accrued and due.</p>
-
-<p>8.2 A reservation will only be confirmed on payment of 50% deposit of the proforma invoice. The 50% balance should be paid upon collecting the car. However,
-full payment can be made for reservations to be confirmed either by cash or credit card should the client choose to.</p>
-
-<p> <strong>9.0	PAYMENT OF RENTAL INVOICES</strong> <br>
-9.1 If any amount due and payable by the client is not paid within the agreed term, the client will be liable for interest at the maximum prevailing market rate.
-Such interest will be calculated and paid monthly in advance. If the interest is not paid as aforesaid, the interest will be added to the principal sum, and the whole
-amount will form the principal debt, which will bear interest as described.</p>
-
-<p>9.2 In the event of City Drive instructing attorneys to collect from the client an amount owing to City Drive, the client agrees to pay all
-costs on the scale as between attorney and own client, including collection charges.</p>
-
-
-<p> <strong>10.0 ESTIMATED TIME OF DELIVERY</strong> <br>
-
-10.1  Our estimated time of delivery is 48hours.This means that if you book a vehicle and get confirmation the latest time within which a vehicle can be availed to a client and hence service delivered is 48hrs/2 days.</p>
-
-<p> <strong>11.0 LIABILITY</strong><br>
-11.1 All City Drive vehicles once rented shall not be used for activities which are prohibited by the laws of Zambia and the company shall not accept any responsibility
-on any of its vehicles used for such activities. All vehicles not returned on time within 24hrs after end of hire and without prior communication of the extension of the
-rental period will be declared stolen.</p>
-
-<p>11.2 All clients are required to read through both the vehicle Rental Contract terms. The acceptance of our offer/service provision on the part of the client and the signing
-of the vehicle rental contract will be full acceptance of our vehicle rental terms and conditions as outlined on the vehicle rental contract form .All Trading conditions and the
-vehicle rental contract terms and conditions shall be construed and enforced according to the laws of the republic of Zambia.</p>
-</article>
-<br class="clear"/>
-
-
-
-			
-			
-			
-               
-			
-			
+<div class="container" style="margin-top:90px;">
+	<div class="card-panel z-depth-5">
+		<div class="row">
+			<h2>TERMS & CONDITIONS</h2> 
 		</div>
-	</div>
-</div>
-
-  
+		<div class="row">
+			<div class="col s12 m12 l12">
+				<?php echo getTerms();?>
+			</div>
+		</div><!--row-->
+	</div><!--card-->
+</div><!--conatiner-->
